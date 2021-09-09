f7e427adbfcd87d11e351caa6649e7c4fd8200dd
Elena Bianca Balaban <elenabiancabal@gmail.com>
2021-09-09 10:27:02 +0200

    PRøvde meg på geolokasjon

--- info.nettleser/nettleser.js
+++ info.nettleser/nettleser.js
@@ -1,20 +1,27 @@
-document.getElementById("utskrift").innerHTML =
- "Fanen du har åpnet er " + window.innerWidth 
- + " piksler brei, og " 
- + window.innerHeight + " piksler høg. Nettsida som er open er " 
- + window.location + "."
-;
+//informasjon man kan finne i nettleseren
+document.getElementById("utskrift").innerHTML = "Fanen du har åpnet er " + window.innerWidth + " piksler brei, og " + window.innerHeight + " piksler høy. Nettsiden som er åpen er " + window.location + "." ;
 
-console.log("Din aplikasjon " + navigator.appName)
+//A Boolean, indicating whether cookies are enabled in the browser.
+//Returns true if enabled, otherwise it returns false
+
 console.log("navigator.cookieEnabled: " + navigator.cookieEnabled);
 
-document.getElementById ("personlig_info").innerHTML = "språket du bruker "
- + navigator.languages + "."
-;
+//viser hvilket språk nettleseren bruker
+document.getElementById ("personlig_info").innerHTML = "språket du bruker" + navigator.languages + "." ;
 
-  // Geolokasjon
-  navigator.geolocation.getCurrentPosition(showPosition);
-  function showPosition(position) {
-      console.log("Latitude: " + position.coords.latitude +
-      "Longitude: " + position.coords.longitude);
+// Geolokasjon
+let x = document.getElementById("x");
+x.innerHTML = "det funker";
+
+function getLocation() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(showPosition);
+  } else {
+    x.innerHTML = "Geolocation is not supported by this browser.";
   }
+}
+
+function showPosition(position) {
+  x.innerHTML = "Latitude: " + position.coords.latitude +"<br>Longitude: " + position.coords.longitude;
+}
+
