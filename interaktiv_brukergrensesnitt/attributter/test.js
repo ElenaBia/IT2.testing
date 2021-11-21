654c8c0144e5aec31f06de5e59fa76fd774036d8
Elena Bianca Balaban <elenabiancabal@gmail.com>
2021-11-21 14:26:54 +0100

    input felt

---
+++ interaktiv_brukergrensesnitt/attributter/test.js
@@ -0,0 +1,63 @@
+
+    /* ----------------------------------------------
+
+                   eksempel s.129-130
+                   onfocus, onchange og oninput
+
+     ----------------------------------------------*/ 
+
+let inpNavn = document.getElementById("inpNavn");
+let txtNavn = document.getElementById("txtNavn");
+inpNavn.oninput = function(){//funksjon som printer ut navne
+    txtNavn.innerHTML = inpNavn.value;
+    
+}
+inpNavn.onchange = function(){
+    //setter verdien til en tom tekst når vi trykker enter
+    inpNavn.value=""; 
+}
+
+inpNavn.onfocus = function(){
+    //når du trykker på inputen så kommer den til fokus
+    console.log("nå er det fokus"); 
+}
+
+    /* ----------------------------------------------
+
+                   eksempel s.131
+                   Attributter
+
+     ----------------------------------------------*/ 
+
+let Navn = document.getElementById("Navn");
+let T_Navn = document.getElementById("T_Navn");
+Navn.oninput = function(){//funksjon som printer ut navne
+    T_Navn.innerHTML = Navn.value;
+
+}
+Navn.onchange = function(){
+    //setter verdien til en tom tekst når vi trykker enter
+    Navn.value=""; 
+}
+
+    /* ----------------------------------------------
+
+                   eksempel s.133
+                   Kalkis
+
+     ----------------------------------------------*/ 
+//definerer variabler
+let tall1 = document.getElementById("tall1");
+let tall2 = document.getElementById("tall2");
+let txtSum = document.getElementById("txtSum");
+
+function leggSammen () {
+    //lager nye variabler for å regne og hente ut info
+    let t1 = Number(tall1.value);
+    let t2 = Number(tall2.value);
+    let sum = t1+t2;
+    //printer summen ut
+    txtSum.innerHTML = sum;
+}
+tall1.oninput = leggSammen;//Starter funksjonen når vi skriver i feltet
+tall2.oninput = leggSammen; //Starter samme funksjon for det andre feltet
